bd4f808ac12193dfa6b747874fc506eda30e2088
Andrew Shaffer <aeshaffer@gmail.com>
2015-01-22 22:42:46 -0600

    Add printing styles to CSS, and big dots.

--- bpui.js
+++ bpui.js
@@ -1150,6 +1150,12 @@ BPWidget.prototype.setup = function() {
     });
 
     this.plotbutton.click(function() {
+
+	    if(parseFloat(that.pixels.val()) > 900) {
+		$("body").addClass("bigdots");
+	    } else {
+		$("body").removeClass("bigdots");
+	    }
 	that.replotMe();
     });
 };
